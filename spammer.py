3dfae3fe000cc77e82c0b46166b0763a1ef21af6
AetherYT <h2110104@nushigh.edu.sg>
2023-08-01 15:55:25 +0800

    fixed some compat issues, and added maximum speed

--- spammer.py
+++ spammer.py
@@ -5,7 +5,7 @@ try:
 except ImportError:
     from pip._internal import main as pip
 print("Installing required libraries...")
-pip.main(["install", "pynput"])
+pip.main(["install", "--user", "pynput"])
 from pynput.mouse import Controller, Button
 from pynput import keyboard
 from pynput.keyboard import Key
@@ -13,13 +13,15 @@ keyboard = keyboard.Controller()
 mouse = Controller()
 spamword = input("Enter the text that you would like to spam:")
 spamtimes = int(input("Enter the number of times you would like to spam the text:"))
-app = input("Select the app that you are using to spam:")
-if app == "Teams" or app == "Microsoft Teams":
+app = input("Select the app that you are using to spam (type max ONLY if you know what you are doing):")
+if app == "Teams" or app == "Microsoft Teams" or app == "MS Teams":
     appspeed = 0.6
 elif app == "WhatsApp" or app == "WA" or app == "Whatsapp" or app == "wa":
     appspeed = 0.2
+elif app == 'max' or 'MAX':
+    appspeed = 0.0
 else:
-    print("Invalid/unsupported app. Make an issue on GitHub to suggest new app support.")
+    print("Invalid/unsupported app. Make an issue on GitHub (AetherYT) to suggest new app support.")
     sys.exit()
 input("PLACE YOUR MOUSE ON TYPING AREA AND PRESS ENTER:")
 cursorpos = Controller().position
